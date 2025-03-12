db6307a30223aa94ab43d458d6e4e24d4d02e132
ParkSoyun <soyp@naver.com>
2025-03-12 10:44:11 +0900

    Section 5: 3. 일급 컬렉션

--- src/main/java/cleancode/minesweeper/tobe/lesson/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/lesson/GameBoard.java
@@ -1,17 +1,16 @@
 package cleancode.minesweeper.tobe.lesson;
 
 import cleancode.minesweeper.tobe.lesson.cell.Cell;
+import cleancode.minesweeper.tobe.lesson.cell.Cells;
 import cleancode.minesweeper.tobe.lesson.cell.EmptyCell;
 import cleancode.minesweeper.tobe.lesson.cell.LandMineCell;
 import cleancode.minesweeper.tobe.lesson.cell.NumberCell;
 import cleancode.minesweeper.tobe.lesson.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.lesson.position.CellPosition;
+import cleancode.minesweeper.tobe.lesson.position.CellPositions;
 import cleancode.minesweeper.tobe.lesson.position.RelativePosition;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class GameBoard {
 
@@ -70,9 +69,8 @@ public class GameBoard {
     }
 
     public boolean isAllCellChecked() {
-        return Arrays.stream(board)
-                .flatMap(Arrays::stream)
-                .allMatch(Cell::isChecked);
+        Cells cells = Cells.from(board);
+        return cells.isAllChecked();
     }
 
     public boolean isInvalidCellPosition(CellPosition cellPosition) {
@@ -84,34 +82,41 @@ public class GameBoard {
     }
 
     public void initializeGame() {
-        int rowSize = getRowSize();
-        int colSize = getColSize();
+        CellPositions cellPositions = CellPositions.from(board);
 
-        for (int row = 0; row < rowSize; row++) {
-            for (int col = 0; col < colSize; col++) {
-                board[row][col] = new EmptyCell();
+        initializeEmptyCells(cellPositions);
+
+        List<CellPosition> landMinePositions = cellPositions.extractRandomPositions(landMineCount);
+        initializeLandMineCells(landMinePositions);
+
+        List<CellPosition> numberPositionCandidates = cellPositions.subtract(landMinePositions);
+        initializeNumberCells(numberPositionCandidates);
+    }
+
+    private void initializeEmptyCells(CellPositions cellPositions) {
+        List<CellPosition> allPositions = cellPositions.getPositions();
+        for (CellPosition position : allPositions) {
+            updateCellAt(position, new EmptyCell());
+        }
+    }
+
+    private void initializeLandMineCells(List<CellPosition> landMinePositions) {
+        for (CellPosition position : landMinePositions) {
+            updateCellAt(position, new LandMineCell());
+        }
+    }
+
+    private void initializeNumberCells(List<CellPosition> numberPositionCandidates) {
+        for (CellPosition candidatePosition : numberPositionCandidates) {
+            int count = countNearbyLandMines(candidatePosition);
+            if (count != 0) {
+                updateCellAt(candidatePosition, new NumberCell(count));
             }
         }
+    }
 
-        for (int i = 0; i < landMineCount; i++) {
-            int landMineCol = new Random().nextInt(colSize);
-            int landMineRow = new Random().nextInt(rowSize);
-            board[landMineRow][landMineCol] = new LandMineCell();
-        }
-
-        for (int row = 0; row < rowSize; row++) {
-            for (int col = 0; col < colSize; col++) {
-                CellPosition cellPosition = CellPosition.of(row, col);
-                if (isLandMineCellAt(cellPosition)) {
-                    continue;
-                }
-                int count = countNearbyLandMines(cellPosition);
-                if (count == 0) {
-                    continue;
-                }
-                board[row][col] = new NumberCell(count);
-            }
-        }
+    private void updateCellAt(CellPosition position, Cell cell) {
+        board[position.getRowIndex()][position.getColIndex()] = cell;
     }
 
     public String getSign(CellPosition cellPosition) {
@@ -130,6 +135,7 @@ public class GameBoard {
     public int getColSize() {
         return board[0].length;
     }
+
     private int countNearbyLandMines(CellPosition cellPosition) {
         int rowSize = getRowSize();
         int colSize = getColSize();
