2428cdbfdfb9ad9dbafe003f956b40075d1dcf65
ParkSoyun <soyp@naver.com>
2025-03-19 00:53:05 +0900

    Test: [minesweeper] (0, 0)에 대해서 상대 위치(-1, -1)을 계산한 좌표는 이동이 불가능한 좌표다. (#2)

--- src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/position/CellPositionTest.java
+++ src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/position/CellPositionTest.java
@@ -1,6 +1,7 @@
 package cleancode.minesweeper.tobe.lesson.minesweeper.board.position;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -22,4 +23,20 @@ class CellPositionTest {
         assertThat(calculatedPositionBy.getRowIndex()).isEqualTo(1);
         assertThat(calculatedPositionBy.getColIndex()).isEqualTo(4);
     }
+
+    @DisplayName("(0, 0)에 대해서 상대 위치(-1, -1)을 계산한 좌표는 이동이 불가능한 좌표다.")
+    @Test
+    void calculatePositionToOutOfRange() {
+        // given
+        CellPosition cellPosition = CellPosition.of(0, 0);
+        RelativePosition relativePosition = RelativePosition.of(-1, -1);
+
+        // when
+
+        //then
+        assertThatThrownBy(() -> cellPosition.calculatePositionBy(relativePosition))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("움직일 수 있는 좌표가 아닙니다.");
+    }
+
 }
