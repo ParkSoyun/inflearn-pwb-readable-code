1e601811b1344de052247b2f4fbb2e0eb3e0bbe8
ParkSoyun <soyp@naver.com>
2025-03-18 23:02:18 +0900

    Test: [minesweeper] 빈 셀과 숫자 셀에 대하여 모든 셀이 open 되었을 때 모든 셀이 체크된 것이 맞는지 확인한다. (#2)

---
+++ src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/cell/CellsTest.java
@@ -0,0 +1,36 @@
+package cleancode.minesweeper.tobe.lesson.minesweeper.board.cell;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CellsTest {
+
+    @DisplayName("빈 셀과 숫자 셀에 대하여 모든 셀이 open 되었을 때 모든 셀이 체크된 것이 맞는지 확인한다.")
+    @Test
+    void isAllEmptyCellNumberCellChecked() {
+        // given
+        List<Cell> cellList = List.of(
+            new EmptyCell(),
+            new NumberCell(1),
+            new EmptyCell(),
+            new NumberCell(2)
+        );
+
+        Cells cells = Cells.of(cellList);
+
+        for(Cell cell : cellList) {
+            cell.open();
+        }
+
+        // when
+        boolean isAllCheckedResult = cells.isAllChecked();
+
+        //then
+        assertThat(isAllCheckedResult).isTrue();
+    }
+
+}
