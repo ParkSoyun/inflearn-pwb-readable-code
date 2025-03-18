fccbd895c9d7f74e8363cf74207b670ffa40a9e2
ParkSoyun <soyp@naver.com>
2025-03-18 23:37:55 +0900

    Test: [minesweeper] (2, 3)에 대해서 상대 위치(-1, 1)을 계산한 결과는 (1, 4)이다. (#2)

---
+++ src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/position/CellPositionTest.java
@@ -0,0 +1,25 @@
+package cleancode.minesweeper.tobe.lesson.minesweeper.board.position;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CellPositionTest {
+
+    @DisplayName("(2, 3)에 대해서 상대 위치(-1, 1)을 계산한 결과는 (1, 4)이다.")
+    @Test
+    void calculatePositionBy() {
+        // given
+        CellPosition cellPosition = CellPosition.of(2, 3);
+        RelativePosition relativePosition = RelativePosition.of(-1, 1);
+
+        // when
+        CellPosition calculatedPositionBy = cellPosition.calculatePositionBy(relativePosition);
+
+        //then
+        assertThat(calculatedPositionBy.getRowIndex()).isEqualTo(1);
+        assertThat(calculatedPositionBy.getColIndex()).isEqualTo(4);
+    }
+}
