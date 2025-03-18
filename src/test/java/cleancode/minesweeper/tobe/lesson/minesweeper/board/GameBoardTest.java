f83daaa42d55a4de8d189e6e63450977b01e2ce8
ParkSoyun <soyp@naver.com>
2025-03-19 01:15:48 +0900

    Refactor: [minesweeper] 각 게임 레벨에 맞는 게임 보드가 생성된다. 테스트 코드 매개 변수 수정 (#2)

--- src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/GameBoardTest.java
+++ src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/GameBoardTest.java
@@ -10,6 +10,7 @@ import cleancode.minesweeper.tobe.lesson.minesweeper.gamelevel.VeryBeginner;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,8 +20,10 @@ class GameBoardTest {
     @ParameterizedTest(name = "[{index}] {0}")
     @MethodSource("gameLevelProvider")
     @DisplayName("각 게임 레벨에 맞는 게임 보드가 생성된다.")
-    void createGameBoardWithGameLevel(GameLevel gameLevel, int expectedRowSize, int expectedColSize) {
+    void createGameBoardWithGameLevel(GameLevel gameLevel) {
         // given
+        int expectedRowSize = gameLevel.getRowSize();
+        int expectedColSize = gameLevel.getColSize();
 
         // when
         GameBoard gameBoard = new GameBoard(gameLevel);
@@ -32,10 +35,10 @@ class GameBoardTest {
 
     private static Stream<Arguments> gameLevelProvider() {
         return Stream.of(
-            Arguments.of(Named.of("Very Beginner", new VeryBeginner()), 4, 5),
-            Arguments.of(Named.of("Beginner", new Beginner()), 8, 10),
-            Arguments.of(Named.of("Middle", new Middle()), 14, 18),
-            Arguments.of(Named.of("Advanced", new Advanced()), 20, 24)
+            Arguments.of(Named.of("Very Beginner", new VeryBeginner())),
+            Arguments.of(Named.of("Beginner", new Beginner())),
+            Arguments.of(Named.of("Middle", new Middle())),
+            Arguments.of(Named.of("Advanced", new Advanced()))
         );
     }
 
