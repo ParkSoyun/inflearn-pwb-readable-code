20d6081ae73c0d1eb37d2ba0e8987c3c510e0be8
ParkSoyun <soyp@naver.com>
2025-03-18 22:26:58 +0900

    Test: [minesweeper] 전체 셀 위치 리스트에서 주어진 셀 위치 리스트를 제외한 리스트를 반환한다. (#2)

--- src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/position/CellPositionsTest.java
+++ src/test/java/cleancode/minesweeper/tobe/lesson/minesweeper/board/position/CellPositionsTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -39,6 +40,40 @@ class CellPositionsTest {
         assertThat(landMinePositions).hasSize(landMineCount);
     }
 
+    @DisplayName("전체 셀 위치 리스트에서 주어진 셀 위치 리스트를 제외한 리스트를 반환한다.")
+    @Test
+    void substract() {
+        // given
+        List<CellPosition> allPositionList = List.of(
+            CellPosition.of(0, 0),
+            CellPosition.of(0, 1),
+            CellPosition.of(0, 2),
+            CellPosition.of(1, 0),
+            CellPosition.of(1, 1),
+            CellPosition.of(1, 2)
+        );
+
+        List<CellPosition> positionListToSubtract = List.of(
+            CellPosition.of(0, 2),
+            CellPosition.of(1, 0)
+        );
+
+        CellPositions cellPositions = CellPositions.of(allPositionList);
+
+        // when
+        List<CellPosition> positionListAfterSubtract = cellPositions.subtract(positionListToSubtract);
+
+        //then
+        assertThat(positionListAfterSubtract).hasSize(4);
+        assertThat(positionListAfterSubtract)
+            .isEqualTo(List.of(
+                CellPosition.of(0, 0),
+                CellPosition.of(0, 1),
+                CellPosition.of(1, 1),
+                CellPosition.of(1, 2)
+            )
+        );
+    }
     private static Stream<Arguments> gameLevelProvider() {
         return Stream.of(
             Arguments.of(Named.of("Very Beginner", new VeryBeginner()), 4, 5),
